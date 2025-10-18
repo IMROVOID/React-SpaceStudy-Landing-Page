74b0fa2919ff7ff70fb17f09f667266e923f3984
ROVOID <141726490+IMROVOID@users.noreply.github.com>
2025-10-18 20:08:27 +0330

    Configured the project for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This is the crucial line for GitHub Pages deployment
+  base: '/React-SpaceStudy-Landing-Page/',
 })
